dc46a96410126fff84358825b5d56e24d96f96e3
akhdede <akhdede@gmail.com>
2019-10-22 13:40:59 +0000

    add articles -> :show

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  resources :articles, only: [:index]
+  resources :articles, only: [:index, :show]
   resources :categories, only: [:index]
   resources :errors, only: [:index]
 
