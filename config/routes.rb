dd06cff0324a43bfe7c9848eed431be2c0d860eb
akhdede <akhdede@gmail.com>
2019-10-30 11:14:13 +0000

    add :new in articles

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  resources :articles, only: [:index, :show]
+  resources :articles, only: [:index, :show, :new]
   resources :categories, only: [:index]
   resources :errors, only: [:index]
 
