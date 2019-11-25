28b0b821b0efd83e7ae2090cdad480cc73bc37db
akhdede <akhdede@gmail.com>
2019-11-25 14:50:23 +0000

    add will_paginate setting

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 6)
     @categories = Category.first(4)
   end
 end
