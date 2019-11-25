a2089f4f15a21ea338303bb9c5d3967d35d8e362
akhdede <akhdede@gmail.com>
2019-11-25 14:50:50 +0000

    add counter_cache

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,9 @@
 class Article < ApplicationRecord
+  extend FriendlyId
+
+  friendly_id :title, use: :slugged
+
   mount_uploader :image_feature, ImageFeatureUploader
 
-  belongs_to :category
+  belongs_to :category, counter_cache: true
 end
