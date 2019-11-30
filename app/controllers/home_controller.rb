9e39595030221b607be42c9933892c10a8b4dba0
akhdede <akhdede@gmail.com>
2019-11-30 06:48:33 +0000

    article order by desc

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @articles = Article.paginate(page: params[:page], per_page: 6)
+    @articles = Article.paginate(page: params[:page], per_page: 6).order(id: :desc)
     @categories = Category.first(4)
   end
 end
