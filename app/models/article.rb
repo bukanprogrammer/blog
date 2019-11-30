39e0769efcfae2ba5d726f69e4762edcd3997bf4
akhdede <akhdede@gmail.com>
2019-11-30 06:49:13 +0000

    add validates

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,13 @@
 class Article < ApplicationRecord
   extend FriendlyId
-
   friendly_id :title, use: :slugged
 
   mount_uploader :image_feature, ImageFeatureUploader
 
   belongs_to :category, counter_cache: true
+
+  validates :title, presence: true, length: {maximum: 25}
+  validates :content, presence: true, length: {minimum: 100}
+  validates :image_feature, presence: true
+  validates :slug, presence: true
 end
