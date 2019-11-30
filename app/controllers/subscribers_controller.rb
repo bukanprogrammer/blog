1abce9998e2fc96442d3b595290dfb1ecc2a26ea
akhdede <akhdede@gmail.com>
2019-11-30 06:55:43 +0000

    add subscribers controller

---
+++ app/controllers/subscribers_controller.rb
@@ -0,0 +1,22 @@
+class SubscribersController < ApplicationController
+  def create
+    @subscriber = Subscriber.new(subscriber_params)
+
+    respond_to do |format|
+
+      if @subscriber.save
+        format.html { redirect_to root_path, notice: 'Anda telah menjadi subscriber' }
+        format.json { render :show, status: :created, location: root_path }
+      else
+        format.html { redirect_to root_path, notice: @subscriber.errors[:email].first  }
+        format.json { render json: @subscriber.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def subscriber_params
+    params.require(:subscriber).permit(:email)
+  end
+end
