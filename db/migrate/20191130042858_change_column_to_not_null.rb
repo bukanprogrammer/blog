8e1aa0104d5840ea78e45cd72607417bb5c50f44
akhdede <akhdede@gmail.com>
2019-11-30 06:59:33 +0000

    change column to not null

---
+++ db/migrate/20191130042858_change_column_to_not_null.rb
@@ -0,0 +1,14 @@
+class ChangeColumnToNotNull < ActiveRecord::Migration[6.1]
+  def change
+    change_column_null :articles, :title, false
+    change_column_null :articles, :content, false
+    change_column_null :articles, :image_feature, false
+    change_column_null :articles, :category_id, false
+    change_column_null :articles, :slug, false
+    change_column_null :categories, :name, false
+    change_column_null :categories, :image_feature, false
+    change_column_null :categories, :slug, false
+    change_column_null :categories, :articles_count, false
+    change_column_null :subscribers, :email, false
+  end
+end
