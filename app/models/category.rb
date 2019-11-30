c2c849d0b6e13d00b516a07e2f6d9a74326cc0e4
akhdede <akhdede@gmail.com>
2019-11-30 06:50:51 +0000

    add validates

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,11 @@
 class Category < ApplicationRecord
   extend FriendlyId
-
   friendly_id :name, use: :slugged
 
   mount_uploader :image_feature, ImageFeatureUploader
 
   has_many :articles
+
+  validates :name, presence: true
+  validates :image_feature, presence: true
 end
