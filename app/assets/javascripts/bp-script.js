1b9b511d4b57cc4756cb51d67aea583378f50f49
akhdede <akhdede@gmail.com>
2019-12-02 16:39:31 +0000

    fix data tables js

--- app/assets/javascripts/bp-script.js
+++ app/assets/javascripts/bp-script.js
@@ -25,5 +25,20 @@ $(document).ready(function() {
 			});
 	}, 2000);
 
-  $('#example').DataTable();
+	$('#example').DataTable( {
+			responsive: {
+					details: {
+							display: $.fn.dataTable.Responsive.display.modal( {
+									header: function ( row ) {
+											var data = row.data();
+											return 'Details for '+data[0]+' '+data[1];
+									}
+							} ),
+							renderer: $.fn.dataTable.Responsive.renderer.tableAll( {
+									tableClass: 'table'
+							} )
+					}
+			}
+	} );
 });
+
