952ba1e6bf0c4ea97e37dd678cca6bf02506c4de
akhdede <akhdede@gmail.com>
2019-12-02 16:42:08 +0000

    add has many articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   # devise :database_authenticatable, :registerable,
   #        :recoverable, :rememberable, :validatable
   devise :database_authenticatable
+
+  has_many :articles
 end
