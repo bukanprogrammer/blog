6e003490a53bd3da6e257e4596d43459e686348f
akhdede <akhdede@gmail.com>
2019-12-10 13:33:13 +0000

    add @article friendly params

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -1,8 +1,13 @@
 module Admin
   class ArticlesController < ActionController::ArticlesController
     before_action :authenticate_user!
+
     def index
       @articles = Article.all.order(id: :desc)
+    end
+
+    def show
+      @article = Article.friendly.find(params[:id])
     end
   end
 end
