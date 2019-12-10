42deb0c91483f2e872330e723be176875e807353
akhdede <akhdede@gmail.com>
2019-12-10 13:35:46 +0000

    add article condition

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,7 +4,7 @@ class Category < ApplicationRecord
 
   mount_uploader :image_feature, ImageFeatureUploader
 
-  has_many :articles
+  has_many :articles, -> { where publish: :y }
 
   validates :name, presence: true
   validates :image_feature, presence: true
