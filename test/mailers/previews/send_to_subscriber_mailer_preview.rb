0f5e9a2112ba0db69a56935d7f28d3c1cfa392ad
akhdede <akhdede@gmail.com>
2019-12-10 13:40:26 +0000

    delete mailer test

--- test/mailers/previews/send_to_subscriber_mailer_preview.rb
+++
@@ -1,4 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/send_to_subscriber_mailer
-class SendToSubscriberMailerPreview < ActionMailer::Preview
-
-end
--- test/mailers/send_to_subscriber_mailer_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SendToSubscriberMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
