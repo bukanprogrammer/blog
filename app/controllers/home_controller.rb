66214512987b8c978d989ca0f3ac34e26362c1a0
akhdede <akhdede@gmail.com>
2019-12-23 07:51:32 +0000

    delete unneccessary file

--- app/controllers/home_controller.rb
+++
@@ -1,6 +0,0 @@
-class HomeController < ApplicationController
-  def index
-    @articles = Article.where(publish: :y).paginate(page: params[:page], per_page: 6).order(id: :desc)
-    @categories = Category.first(4)
-  end
-end
