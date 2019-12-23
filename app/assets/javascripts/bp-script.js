00a396a324bb90bb1f620c2e3b7231a812fcd28b
akhdede <akhdede@gmail.com>
2019-12-23 14:32:07 +0000

    fix script js

--- app/assets/javascripts/bp-script.js
+++ app/assets/javascripts/bp-script.js
@@ -1,26 +1,26 @@
 $(document).ready(function() {
   // auto hide notif
 	window.setTimeout(function() {
-			$(".alert").fadeTo(500, 0).slideUp(500, function(){
-					$(this).remove(); 
-			});
+    $(".alert").fadeTo(500, 0).slideUp(500, function(){
+        $(this).remove(); 
+    });
 	}, 2000);
 
   // data table
 	$('#example').DataTable( {
-			responsive: {
-					details: {
-							display: $.fn.dataTable.Responsive.display.modal( {
-									header: function ( row ) {
-											var data = row.data();
-											return 'Details for '+data[0]+' '+data[1];
-									}
-							} ),
-							renderer: $.fn.dataTable.Responsive.renderer.tableAll( {
-									tableClass: 'table'
-							} )
-					}
-			}
+    responsive: {
+        details: {
+            display: $.fn.dataTable.Responsive.display.modal( {
+                header: function ( row ) {
+                    var data = row.data();
+                    return 'Details for '+data[0]+' '+data[1];
+                }
+            } ),
+            renderer: $.fn.dataTable.Responsive.renderer.tableAll( {
+                tableClass: 'table'
+            } )
+        }
+    }
 	} );
 
 });
@@ -74,6 +74,8 @@ $(document).on('turbolinks:load', function () {
       $(this).removeClass('shadow-sm text-dark font-weight-bold');
     }
   );
+
+  tinymce.remove();
 
   tinymce.init({
     selector: "textarea",  // change this value according to your HTML
