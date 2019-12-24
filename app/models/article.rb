96955407627f3cdd99d4b8c2161dfbfebd4ab912
akhdede <akhdede@gmail.com>
2019-12-24 18:23:45 +0000

    add tag relation & tag method

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,11 +8,29 @@ class Article < ApplicationRecord
 
   belongs_to :user
 
+  has_many :taggings, dependent: :destroy
+
+  has_many :tags, through: :taggings
+
   validates :title, presence: true, length: {maximum: 50}
   validates :content, presence: true, length: {minimum: 100}
   validates :image_feature, presence: true
   validates :slug, presence: true
 
   acts_as_punchable
+
+  def self.tagged_with(name)
+    Tag.find_by!(name: name).articles
+  end
+
+  def all_tags=(names)
+    self.tags = names.split(',').map do |name|
+      Tag.where(name: name).first_or_create!
+    end
+  end
+
+  def all_tags
+    tags.map(&:name).join(", ")
+  end
 
 end
