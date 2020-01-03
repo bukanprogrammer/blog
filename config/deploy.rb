d2073e4ca583739ac162a7c0ad8068cbd29f11e9
akhdede <akhdede@gmail.com>
2020-01-03 14:33:30 +0000

    add folder

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,6 +39,3 @@ set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
-task :symlink_config, roles: :app do
-  run "ln -nfs #{shared_path}/public/resources/images/uploads #{release_path}/public/resources/images/uploads"
-end
