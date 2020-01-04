28c0b855ae3d81b2b19ebfe176eecd0e10714fcd
akhdede <akhdede@gmail.com>
2020-01-04 07:19:16 +0000

    change image_feature to image

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -80,6 +80,6 @@ class CategoriesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def category_params
-      params.require(:category).permit(:name, :image_feature)
+      params.require(:category).permit(:name, :image)
     end
 end
